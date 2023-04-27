35c2ea88d837cddc2e8e24656151c9ab839454ca
ukadner <ulf.kadner@t-online.de>
2023-04-27 10:57:57 +0200

    Add WeatherViewModel

---
+++ app/src/main/java/de/kadner/meinwetter/model/WeatherViewModel.kt
@@ -0,0 +1,43 @@
+package de.kadner.meinwetter.model
+
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import de.kadner.meinwetter.repository.WeatherRepository
+import kotlinx.coroutines.launch
+
+class WeatherViewModel( val app: Context ) : ViewModel()
+{
+
+    val _weatherData = MutableLiveData<WeatherData>()
+    val _weatherImage = MutableLiveData<Bitmap>()
+
+    val weatherRepository = WeatherRepository( app )
+    val weatherData: LiveData<WeatherData> = _weatherData
+    val weatherImage: LiveData<Bitmap> = _weatherImage
+
+
+    fun getWeatherData( city: String )
+    {
+
+        viewModelScope.launch()
+        {
+            _weatherData.value = weatherRepository.getWeather( city )
+        }
+
+    }
+
+    fun getImage()
+    {
+
+        viewModelScope.launch()
+        {
+            weatherRepository.getImage( _weatherImage, weatherData.value!! )
+        }
+
+    }
+
+}
